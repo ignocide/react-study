da9b89c947d1c9190788c8904d530b0c0a9c152f
morse <morse@madsq.net>
2016-11-13 12:04:38 +0900

    * react project init

--- src/item/extend.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Extend from '../components/Extend'
-
-const extendElement = document.getElementById('extends')
-ReactDOM.render(<Extend />, extendElement)
